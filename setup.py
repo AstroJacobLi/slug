07151d55fb103cff97cc8ceaf6851c8fe8ca5077
Jiaxuan <jiaxuan_li@pku.edu.cn>
2018-11-09 17:28:44 -0800

    upload setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='slug', version='0.1',
+	description='Integrated tool for calculating 1-D surface brightness profile of massive galaxies',
+	url='https://github.com/AstroJacobLi/slug',
+	author='Jiaxuan Li',
+	author_email='jiaxuan_li@pku.edu.cn',
+	license='MIT',
+	packages=['slug'],
+	zip_safe=False)
